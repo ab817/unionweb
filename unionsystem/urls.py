bfbd173bc2a578405193b58ccb8e675e168e5b12
bombk <bomncit@gmail.com>
2025-05-19 10:57:36 +0545

    static medial root added

--- unionsystem/urls.py
+++ unionsystem/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('unionapp.urls')),  # Routes the homepage to unionapp
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
