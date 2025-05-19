11b29ff0de53570f5cbedd22deba42db75350fc5
bombk <bomncit@gmail.com>
2025-05-19 10:50:13 +0545

    debug false

--- unionsystem/urls.py
+++ unionsystem/urls.py
@@ -22,6 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('unionapp.urls')),  # Routes the homepage to unionapp
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
