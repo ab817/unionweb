b54b7a96d2a6cfef6dd83bc1020fd57aa7e0d5a1
bombk <bomncit@gmail.com>
2025-05-19 10:59:07 +0545

    revert change

--- unionsystem/urls.py
+++ unionsystem/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('unionapp.urls')),  # Routes the homepage to unionapp
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
